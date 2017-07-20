22b19e0eb841d4dcadbcf9dda16be7dc7813bc13
gvnmccld <cannonc@gmail.com>
2017-07-20 09:51:32 -0700

    Forgot to rev nuget again.

--- src/log4net.ElasticSearch/AssemblyVersionInfo.cs
+++ src/log4net.ElasticSearch/AssemblyVersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.3.4.5")]
-[assembly: AssemblyFileVersion("2.3.4.5")]
+[assembly: AssemblyVersion("2.3.4.6")]
+[assembly: AssemblyFileVersion("2.3.4.6")]
