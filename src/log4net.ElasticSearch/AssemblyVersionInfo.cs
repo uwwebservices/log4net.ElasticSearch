a85b9c14f06862164e236a5875fc958066f130e6
gvnmccld <cannonc@gmail.com>
2017-07-28 11:20:11 -0700

    updating versions

--- src/log4net.ElasticSearch/AssemblyVersionInfo.cs
+++ src/log4net.ElasticSearch/AssemblyVersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.3.4.7")]
-[assembly: AssemblyFileVersion("2.3.4.7")]
+[assembly: AssemblyVersion("2.3.4.8")]
+[assembly: AssemblyFileVersion("2.3.4.8")]
